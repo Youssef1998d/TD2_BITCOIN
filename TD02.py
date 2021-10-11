43ffcfcde4359053c52badad30597150814d966c
Youssef1998d <50227410+Youssef1998d@users.noreply.github.com>
2021-10-11 18:18:07 +0200

    Update TD02.py

--- TD02.py
+++ TD02.py
@@ -25,11 +25,33 @@ print("\n\n")
 
 from backports.pbkdf2 import pbkdf2_hmac
 import binascii 
-key = pbkdf2_hmac("sha512", mnemonic.encode("utf8"), "mnemonic".encode("utf8"), 2048, 256)
-print("key:\n", str(binascii.hexlify(key))[2:514])
-MasterPrivatekey = str(binascii.hexlify(key))[2:258]
+key = pbkdf2_hmac("sha512", mnemonic.encode("utf8"), "mnemonic".encode("utf8"), 2048, 128)
+print("key:\n", str(binascii.hexlify(key))[2:130])
+MasterPrivatekey = str(binascii.hexlify(key))[2:66]
 print("MasterPrivatekey:\n", MasterPrivatekey, len(MasterPrivatekey))
-MasterChainCode = str(binascii.hexlify(key))[258:514]
+MasterChainCode = str(binascii.hexlify(key))[66:130]
 print("MasterChainCode:\n", MasterChainCode, len(MasterChainCode))
 print("\n\n")
+
+
+#Master Public Key & child key
+from mnemonic import Mnemonic
+import bip32utils
+
+mnemon = Mnemonic('english')
+seed = mnemon.to_seed(b'athlete fiscal prize organ crush artefact elbow three winter never obey angry')
+print(f'BIP39 Seed: {seed.hex()}\n')
+root_key = bip32utils.BIP32Key.fromEntropy(seed)
+root_public_hex = root_key.PublicKey().hex()
+#Master Public Key
+print(f'\tMaster Public Key: {root_public_hex}') 
+#child key
+child_key = root_key.ChildKey(0).ChildKey(0)
+child_public_hex = child_key.PublicKey().hex()
+print(f'\tChild Public : {child_public_hex}')
+
+#Child key index 13
+child_key = root_key.ChildKey(13).ChildKey(13)
+child_public_hex = child_key.PublicKey().hex()
+print(f'\tChild 13 Public : {child_public_hex}')
 
